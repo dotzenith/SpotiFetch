0ba63c2a0e4460df653afee371105b25333c2024
ZenithDS <sharmadanshu01@gmail.com>
2022-03-05 20:53:50 -0500

    cleaned up some of the logic for currently playing stats

--- SpotiFetch/helpers.py
+++ SpotiFetch/helpers.py
@@ -9,16 +9,16 @@ def create_spotify(scope):
 def get_currently_playing_stats(spotify_obj):
     
     currently_playing = spotify_obj.current_user_playing_track()
+    
+    if currently_playing is not None:
+        return {'artist_name' : currently_playing['item']['artists'][0]['name'],
+                'album_name' : currently_playing['item']['album']['name'],
+                'track_name' : currently_playing['item']['name'],
+                'track_progress' : currently_playing['progress_ms'],
+                'track_duration' : currently_playing['item']['duration_ms'] 
+        }
 
-    artist_name = currently_playing['item']['artists'][0]['name']
-    album_name = currently_playing['item']['album']['name']
-    track_name = currently_playing['item']['name']
-    track_progress = currently_playing['progress_ms']
-    track_duration = currently_playing['item']['duration_ms']
+    return None
 
-    return {'artist_name' : artist_name,
-            'album_name' : album_name,
-            'track_name' : track_name,
-            'track_progress' : track_progress,
-            'track_duration' : track_duration
-    }
+def get_user_stats():
+    pass
