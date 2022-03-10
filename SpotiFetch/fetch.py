057c8b54ec1dee04cf6cda27ac84ec230ca62704
ZenithDS <sharmadanshu01@gmail.com>
2022-03-09 22:53:31 -0500

    full implementation of themes

--- SpotiFetch/fetch.py
+++ SpotiFetch/fetch.py
@@ -3,7 +3,7 @@ from SpotiFetch.colors import colors
 import random
 from rich import print
 
-def main(random_color=False):
+def main(colorscheme="catppuccin", random_color=True):
     Spotipy = create_spotify("user-read-currently-playing user-top-read user-read-recently-played user-read-private")
     
     current_user = get_current_user_info(Spotipy)
@@ -14,7 +14,7 @@ def main(random_color=False):
     '''
     now_playing = get_currently_playing_stats(Spotipy)
     if now_playing is None:
-        now_playing = "Now currently playing track"
+        now_playing = "No currently playing track"
     else:
         now_playing = f"{now_playing['track_name']} - {now_playing['artist_name']}"
 
@@ -22,22 +22,22 @@ def main(random_color=False):
     top_track = get_user_top_track(Spotipy)
     top_artist = get_user_top_artist(Spotipy)
     
-    theme = colors['catppuccin']
+    theme = colors[colorscheme]
 
     if not random_color:
         logo_color = theme['colorFour']
-    elif random_color:
-        logo_color = random.choice([theme.values()]) 
+    else:
+        logo_color = random.choice(list(theme.values()))
 
-    new_art = f"      [logo_color]______[/logo_color]" \
-              f"\n   [logo_color];;        ;;[/logo_color]" \
-              f"\n [logo_color];;            ;;[/logo_color]      [logo_color]USER[/logo_color]            {current_user}" \
-              f"\n[logo_color];;   _..**.._   ;;[/logo_color]     [blue]NOW PLAYING[/blue]     {now_playing}" \
-              f"\n[logo_color];;   _..**.._   ;;[/logo_color]     [magenta]RECENT TRACK[/magenta]    {recently_played['track_name']} - {recently_played['artist_name']}" \
-              f"\n[logo_color];;   _..**.._   ;;[/logo_color]     [yellow]TOP TRACK[/yellow]       {top_track['track_name']} - {top_track['artist_name']}" \
-              f"\n [logo_color];;            ;;[/logo_color]      [cyan]TOP ARTIST[/cyan]      {top_artist}" \
-              f"\n   [logo_color];;        ;;[/logo_color]" \
-              f"\n      [logo_color]------[/logo_color]" \
+    new_art = f"      [{logo_color}]______[/{logo_color}]" \
+              f"\n   [{logo_color}];;        ;;[/{logo_color}]" \
+              f"\n [{logo_color}];;            ;;[/{logo_color}]      [{theme['colorOne']}]USER[/{theme['colorOne']}]            [{theme['fg']}]{current_user}[/{theme['fg']}]" \
+              f"\n[{logo_color}];;   _..**.._   ;;[/{logo_color}]     [{theme['colorTwo']}]NOW PLAYING[/{theme['colorTwo']}]     [{theme['fg']}]{now_playing}[/{theme['fg']}]" \
+              f"\n[{logo_color}];;   _..**.._   ;;[/{logo_color}]     [{theme['colorThree']}]RECENT TRACK[/{theme['colorThree']}]    [{theme['fg']}]{recently_played['track_name']} - {recently_played['artist_name']}[/{theme['fg']}]" \
+              f"\n[{logo_color}];;   _..**.._   ;;[/{logo_color}]     [{theme['colorFour']}]TOP TRACK[/{theme['colorFour']}]       [{theme['fg']}]{top_track['track_name']} - {top_track['artist_name']}[/{theme['fg']}]" \
+              f"\n [{logo_color}];;            ;;[/{logo_color}]      [{theme['colorFive']}]TOP ARTIST[/{theme['colorFive']}]      [{theme['fg']}]{top_artist}[/{theme['fg']}]" \
+              f"\n   [{logo_color}];;        ;;[/{logo_color}]" \
+              f"\n      [{logo_color}]------[/{logo_color}]" \
 
     print(new_art)
 
